f3715111ac9991d7677a6245945517c26cb1b57a
Alex Tait <taittwin13@gmail.com>
2019-02-13 19:18:23 -0800

    Update docuTest.php

--- Backend Code/docuTest.php
+++ Backend Code/docuTest.php
@@ -8,16 +8,53 @@
     require __DIR__ . '/vendor/autoload.php';
     
     try{
+        $target_dir = "uploads/";
+    
+        $baseImPath= basename($_FILES["photo"]["name"]) . date("Y-m-d-H:i:s");
+        $target_file = $target_dir . $baseImPath . '.png';
+        
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+        
         error_reporting(E_ALL);
-        $log = fopen("php.log", "a") or die("Unable to open file!");
+        $log = fopen("php.log", "w") or die("Unable to open file!");
         
         $text = "Begin Code " . date("D M d, Y G:i") . "\n";
         fwrite($log, $text);
         
-        $path = 'https://cs425.alextait.net/serial1.png';
+        $path = 'https://cs425.alextait.net/';
 
-        $im = imagecreatefromstring(file_get_contents($path));
+        // $im = imagecreatefromstring(file_get_contents($path));
 
+        
+
+        $im = imagecreatefromstring(file_get_contents($_FILES["photo"]["tmp_name"]));
+        
+        if($_FILES["photo"]["type"] == "image/jpeg")
+        {
+            $exif = exif_read_data($_FILES['photo']['tmp_name']);
+            if($exif !== false)
+            {
+                if (!empty($exif['Orientation'])) {
+                    switch ($exif['Orientation']) {
+                        case 3:
+                        $im = imagerotate($im, 180, 0);
+                        break;
+                        case 6:
+                        $im = imagerotate($im, -90, 0);
+                        break;
+                        case 8:
+                        $im = imagerotate($im, 90, 0);
+                        break;
+                        default:
+                        $im = $im;
+                    } 
+                }
+            }
+        }
+        imagepng($im, $target_file);
+
+
+        // $im = imagecreatefromstring(file_get_contents($target_file));
         // if( $im !== false)
         // {
         //     // header('Content-Type: image/png');
@@ -28,19 +65,28 @@
         $config = ['credentials' => 'cs425-9bcc7a946012.json',    
                 ];
         
-        //$context = ['languageHints' => 'en'];
+        
         $imageAnnotator = new ImageAnnotatorClient($config);
         
         #annotate the image
-        $response = $imageAnnotator->documentTextDetection($path);
+        $response = $imageAnnotator->documentTextDetection($path . $target_file);
         
         $annotation = $response->getFullTextAnnotation();
+        
+        if($annotation === null)
+        {
+            // print_r($response);
+            //print_r($target_file);
+            //print_r('annotation is null');
+        }
 
         // printf('%d texts found:' . PHP_EOL, count($texts));
+        
         
         $yellow = imagecolorallocate($im, 255, 255, 0);
         imagesetthickness ( $im, 10 );
         $output = '{ "Strings": [';
+        
         
         foreach($annotation->getPages() as $page)
         {
@@ -53,8 +99,17 @@
                         $wordText = '';
                         foreach($word->getSymbols() as $symbol)
                         {
-                            $wordText .= $symbol->getText();
+                            if($symbol->getText() == '"')
+                            {
+                                $wordText .= "\\" . $symbol->getText();
+                            }
+                            else
+                            {
+                                $wordText .= $symbol->getText();
+                            }
+                                
                         }
+                        //$wordText = str_replace('"', $wordText, '\"');
                         $output = $output . '{"Word": "' . ($wordText) . '",';
                         $output = str_replace("\n", " ", $output);
                         
@@ -76,34 +131,26 @@
                 }
             }
         }
-         $savePath = 'serialBoxed.png';
+        
+        
         $output = rtrim($output, ',');
-        //Convert image into byte stream to export to html
-        //ob_start();
-        imagepng($im, $savePath);
-        //$imStream = ob_get_clean();
-        //$src = 'data:image/png;base64,'.base64_encode($imStream);
-        
-       
-        
-        //file_put_contents($savePath, base64_encode($imStream));
-        
-        
-        //echo '<img src="' .$src .'" height=250/>';
-        //echo '<br>';
+        $boxedPath = $target_dir . $baseImPath . 'Boxed.png';
+        imagepng($im, $boxedPath);
         
         //echo output as json object
         $output = $output . "],";
         
-        $output = $output . '"imageURL": "https://cs425.alextait.net/' . $savePath . '"}';
-        echo($output);
+        $output = $output . '"imageURL": "https://cs425.alextait.net/' . $boxedPath . '"}';
+       
         $imageAnnotator->close();
-        
+        fwrite($log, $output);
         $text = "End Code " . date("D M d, Y G:i") . "\n";
         fwrite($log, $text);
+        echo($output);
 
     } catch (Exception $e) {
         echo 'Caught exception: ',  $e->getMessage(), "\n";
+        fwrite($log, 'Caught exception: ');
     }
     
 ?>

4994ad0648d575be3a416bbf6aaf9836f69481c7
Alex Tait <taittwin13@gmail.com>
2019-04-28 15:55:06 -0700

    Added ability to delete an asset and add new entries to an asset

---
+++ Backend Code/deleteEntry.php
@@ -0,0 +1,26 @@
+<?php
+
+    //Start Connection to mySQL server
+    $connection = new mysqli("localhost", "alextait", "1phO3a17gX", "alextait_Cs425");
+  
+    //Check if successful
+    if(!$connection)
+    {
+        die("Connection failed:" . mysqli_connect_error());
+    }
+
+
+    //Insert POST values into database
+    $sql = 'DELETE FROM Assets WHERE creationTime= "' . $_POST["creationTime"] . '";';
+        
+    //Check if successful    
+    if($connection->query($sql) === TRUE)
+    {
+        print "Record Deleted " . $sql;
+    }
+    else
+    {
+        print "Error: " . $sql . "<br>" . $connection->error;
+    }
+
+?>
